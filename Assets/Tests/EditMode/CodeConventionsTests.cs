346b14e279d572b49486bd6728b4bc86e1039faa
rur1a <rubishiner@gmail.com>
2024-06-23 20:43:30 +0200

    add simple test for namespaces check

---
+++ Assets/Tests/EditMode/CodeConventionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+
+namespace Tests.EditMode
+{
+    public class CodeConventionsTests
+    {
+        [Test]
+        public void AllClassesShouldBeInNamespaces()
+        {
+            const string sourceCode = "namespace N {class C {}}";
+
+            var classesNotInNamespaces =
+                CSharpSyntaxTree
+                    .ParseText(sourceCode)
+                    .GetRoot()
+                    .DescendantNodesAndSelf()
+                    .OfType<ClassDeclarationSyntax>()
+                    .Where(NotInNamespace);
+
+            classesNotInNamespaces.Should().BeEmpty();
+        }
+
+        private bool NotInNamespace(ClassDeclarationSyntax node) => !InNamespace(node);
+
+        private bool InNamespace(ClassDeclarationSyntax node) =>
+            node
+                .Ancestors()
+                .OfType<NamespaceDeclarationSyntax>()
+                .Any();
+    }
+}
