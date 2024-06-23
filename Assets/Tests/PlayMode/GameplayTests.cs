015706a1a1a641d5e1705a8d1b7727e086bea3d2
rur1a <rubishiner@gmail.com>
2024-06-23 14:37:41 +0200

    add playmode tests base

---
+++ Assets/Tests/PlayMode/GameplayTests.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode
+{
+    public class GameplayTests
+    {
+        [UnityTest]
+        public IEnumerator GameplayTestsWithEnumeratorPasses()
+        {
+            yield return null;
+        }
+    }
+}
