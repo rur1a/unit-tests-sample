06a48b6edf9ed3d66868832c4adb314564706111
rur1a <rubishiner@gmail.com>
2024-06-23 14:53:13 +0200

    add scene operating test

--- Assets/Tests/PlayMode/GameplayTests.cs
+++ Assets/Tests/PlayMode/GameplayTests.cs
@@ -1,6 +1,8 @@
 using System.Collections;
+using System.Xml.Schema;
 using FluentAssertions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace Tests.PlayMode
@@ -16,6 +18,18 @@ namespace Tests.PlayMode
             //ASSERT
             Time.deltaTime.Should().BePositive();
         }
+
+        [UnityTest]
+        public IEnumerator WhenGameSceneLoaded_ThenItContainsFactoryManager()
+        {
+            //ARRANGE
+            var sceneLoading = SceneManager.LoadSceneAsync("_Scenes/game", LoadSceneMode.Single);
+            yield return new WaitUntil(()=>sceneLoading.isDone);
+            
+            //ASSERT
+            Object.FindObjectOfType<FactoryManager>().Should().NotBeNull();
+        }
+
 
     }
 }
